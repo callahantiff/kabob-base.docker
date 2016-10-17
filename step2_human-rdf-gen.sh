49e0adf713faeffc712b150a442b5d398aec34e6
bill-baumgartner <bill.baumgartner@gmail.com>
2016-10-17 10:43:41 -0600

    added explicit names to rdf-gen containers

--- step2_human-rdf-gen.sh
+++ step2_human-rdf-gen.sh
@@ -16,10 +16,11 @@ declare -a DATASOURCES=("HGNC,NCBIGENE_GENEINFO,NCBIGENE_REFSEQUNIPROTCOLLAB,GOA
 "UNIPROT_IDMAPPING")
 
 DID=""
-
+COUNTER=1
 for ds in "${DATASOURCES[@]}"
 do
     echo "Starting kabob-base container to process: $ds"
-    DID=$DID" "`docker run -d --volumes-from kabob_data ccp/kabob-base:0.1 ./ice-rdf-gen.sh "$TAX" "$ds"`
+    DID=$DID" "`docker run -d --name "rdf_gen_$COUNTER" --volumes-from kabob_data ccp/kabob-base:0.1 ./ice-rdf-gen.sh "$TAX" "$ds"`
+    OUNTER=$((COUNTER + 1))
 done
 docker wait $DID
