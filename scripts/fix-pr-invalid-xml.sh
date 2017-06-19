12c86eabc7857375b64ca3586fb059473907e50f
bill-baumgartner <bill.baumgartner@gmail.com>
2017-06-19 12:09:15 -0600

    Removed no longer needed pr.owl fix

--- scripts/fix-pr-invalid-xml.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#
-
-# This fixes some ampersands that need to be escaped in the pr.owl file
-echo "Fixing invalid XML in pr.owl..."
-sed -i "s/\&name/\&amp;name/g" /kabob_data/ontology/pr.owl
