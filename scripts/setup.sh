c137bf3e6753228cd549c1ae4d4a125f8b8dbc66
bill-baumgartner <bill.baumgartner@gmail.com>
2017-06-21 19:47:46 -0600

    Reordered calls in setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -4,5 +4,6 @@
 mkdir -p /kabob_data/logs
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-$SCRIPT_DIR/download-ontologies.sh
-$SCRIPT_DIR/other-downloads.sh
+${SCRIPT_DIR}/other-downloads.sh
+${SCRIPT_DIR}/download-ontologies.sh
+
